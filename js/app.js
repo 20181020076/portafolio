c25dedd4d53a23fb1a95fb659986e28989378aab
20181020076 <diegoarojas@hotmail.es>
2022-08-17 10:54:13 -0500

    menu desplegable para movile agregado

--- js/app.js
+++ js/app.js
@@ -1,3 +1,20 @@
-function menu(){
-    console.log("hola")
-}
+// const btnMenu = document.querySelector("#btnMenu");
+
+
+
+// btnMenu.addEventListener("click", ()=>{
+//     let men = document.getElementsByClassName("menu__desplegable")
+//     men.style
+// });
+ function changeStyle(){
+    // let element = document.getElementsByClassName("menu__desplegable");
+    // element.height = "500px";
+    // element.overflow = "visible";
+    let element = document.getElementById("menu")
+    let comp = element.currentStyle || getComputedStyle(element, null);
+        if (comp.height == "0px"){
+        element.setAttribute('style', 'height: 130px')
+    }else{
+        element.setAttribute('style', 'height: 0px')
+    }
+};
